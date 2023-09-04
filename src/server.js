3672163b298e75256725df1fba251e7783ae196b
Minwoo02 <onyx9250@naver.com>
2023-09-04 16:48:58 +0900

    Install Morgan

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+app.use(logger);
 
 const handleListening = () => {
   console.log(`✅ Server listening on port http://localhost:${PORT}`);
