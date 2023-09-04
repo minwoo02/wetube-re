66a21ad018c02a8e55abfeaa95c175cbc30594ac
Minwoo02 <onyx9250@naver.com>
2023-09-04 17:17:21 +0900

    Add controllers (and to use Exports)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,11 +1,9 @@
 import express from "express";
+import { watch, edit } from "../controllers/videoControllers";
 
 const videoRouter = express.Router();
 
-const handleWatchVideo = (req, res) => {
-  res.send("Watch Video");
-};
-
-videoRouter.get("/", handleWatchVideo);
+videoRouter.get("/watch", watch);
+videoRouter.get("/edit", edit);
 
 export default videoRouter;
