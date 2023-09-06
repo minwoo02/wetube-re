5ef54f72eae10d9e06ce3b7c6e40686c5dcb71ac
Minwoo02 <onyx9250@naver.com>
2023-09-06 18:04:17 +0900

    Create Video

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -13,7 +13,7 @@ export const home = (req, res) => {
 // 방법2: promise
 export const home = async (req, res) => {
   try {
-    const videos = await Video.find({});
+    const videos = await Video.find({}); //db에 Video 파일들을 불러옴
     return res.render("home", { pageTitle: "Home", videos });
   } catch (error) {
     return res.render("server-error"), { error };
@@ -40,7 +40,40 @@ export const getUplaod = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 
-export const postUpload = (req, res) => {
-  const { title } = req.body;
+export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    creadAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
+  // 새로운 video object 생성시, mongoose가 고유 _id를 부여해준다.
+  // **JS 내에서만 존재함.**
+  const dbVideo = await video.save(); //데이터를 db에 전송하는데 시간이 걸리기 떄문에 await으로 기다려줘야함
+  // **db에도 존재함.**
+  console.log(dbVideo);
   return res.redirect("/");
 };
+
+// Model.create() 사용시 create() 다음 미들웨어인 save()를 트리거.
+
+/* export const postUpload = async (req, res) => {
+  const { title, description, hashtags } = req.body;
+  await Video.create({
+    title,
+    description,
+    creadAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  return res.redirect("/");
+}; */
