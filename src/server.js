dacba604712c22879090ea5ce31acb846d849fd7
Minwoo02 <onyx9250@naver.com>
2023-09-13 19:02:59 +0900

    Sessions and Cookies 1

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@
 
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -13,6 +14,22 @@ app.use(logger);
 app.set("view engine", "pug"); //현재 작업 디렉토리에서, express가 views 디렉토리에서 pug파일을 찾도록 설정
 app.set("views", process.cwd() + "/src/views"); //views 경로지정, express 랜더링
 app.use(express.urlencoded({ extended: true })); //express application이 form의 value를 이해할 수 있도록
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
