8d1eb42c2f12c12ba092ec7e88367b8992364437
Minwoo02 <onyx9250@naver.com>
2023-09-15 00:53:14 +0900

    Protector and Public Middlewares

--- src/middlewares.js
+++ src/middlewares.js
@@ -5,3 +5,21 @@ export const localsMiddleware = (req, res, next) => {
   console.log(res.locals);
   next();
 };
+
+/*--loggedIn되지 않으면 `/login`으로 redirect, 즉 로그인되어있는 사람만 접근 --*/
+export const protectorMiddleware = (req, res, next) => {
+  if (req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/login");
+  }
+};
+
+/*--loggedIn되면 `/`으로 redirect, 즉 로그인된 사람은 `/`으로 리다이렉트 --*/
+export const publicOnlyMiddleware = (req, res, next) => {
+  if (!req.session.loggedIn) {
+    return next();
+  } else {
+    return res.redirect("/");
+  }
+};
