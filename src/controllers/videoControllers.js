3ec63edaff5de8d6c136cfac212c588884d8fe59
Minwoo02 <onyx9250@naver.com>
2023-09-16 06:55:47 +0900

    Video Owner

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,4 +1,5 @@
 import Video from "../models/Video.js";
+import User from "../models/User";
 
 export const home = async (req, res) => {
   try {
@@ -11,7 +12,7 @@ export const home = async (req, res) => {
 
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id); //db에서 해당 id에 Video 파일을 불러옴, pug 때문
+  const video = await Video.findById(id).populate("owner");
   if (!video) {
     return res.render("404", { pageTitle: "Video not found." });
   }
@@ -52,6 +53,9 @@ export const getUplaod = (req, res) => {
 };
 
 export const postUpload = async (req, res) => {
+  const {
+    user: { _id },
+  } = req.session;
   const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
@@ -59,6 +63,7 @@ export const postUpload = async (req, res) => {
       title,
       description,
       fileUrl,
+      owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
